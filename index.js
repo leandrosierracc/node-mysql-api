0397bcb91fee25edc25ea487027462e233a66af3
A <leandro.sierra@tarify.com>
2024-10-14 11:39:42 +0200

    Enlazar con mysql

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // para poder usar export/import en package.js poner lo de  "type": "module",
 import express from "express"
+import {pool} from "./db.js"
 
 const app = express()
+
+app.get("/probarCnn", async (req,res) => {
+    const result = await pool.query("select * from clientes")
+    res.json(result)
+})
 
 app.listen(3000)
 console.log("server funcionando en 3000")
